68e1da63c327f35532de2e4f545c3b1e8af3fa2f
muzzamil176 <139631310+muzzamil176@users.noreply.github.com>
2024-09-20 21:31:22 +0500

    Add files via upload

---
+++ inventory-service/inventory_service/model.py
@@ -0,0 +1,22 @@
+from sqlmodel import SQLModel,Field
+from typing import Optional
+
+
+
+class Products (SQLModel, table=True):
+    id:Optional[int] = Field(default= None, primary_key= True)
+    name: str = Field()
+    price:float = Field()
+    quantity : int = Field() 
+    
+
+
+class inventory_item(SQLModel):
+    product_id : int = Field()
+    quantity : int = Field()
+    
+
+
+
+
+
